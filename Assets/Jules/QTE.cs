e19ee5fc43810c913bd88fddf7e11c4b1d6be69a
Drawnight01 <71324732+Drawnight01@users.noreply.github.com>
2023-02-05 03:36:42 +0100

    alottaothings

--- Assets/Jules/QTE.cs
+++ Assets/Jules/QTE.cs
@@ -20,9 +20,20 @@ public class QTE : MonoBehaviour
 
     private Sprite currentGeneratedInput;
     private Sprite X_Btn, Y_Btn, B_Btn, A_Btn;
-    public Image FirstBtn, SecondBtn, ThirdBtn, FourthBtn, FirstBtn1, SecondBtn1, ThirdBtn1, FourthBtn1;
+    private Image FirstBtn, SecondBtn, ThirdBtn, FourthBtn, FirstBtn1, SecondBtn1, ThirdBtn1, FourthBtn1;
+
+    private Image BackFirstBtn,
+        BackSecondBtn,
+        BackThirdBtn,
+        BackFourthBtn,
+        BackFirstBtn1,
+        BackSecondBtn1,
+        BackThirdBtn1,
+        BackFourthBtn1;
 
     private List<Image> QTEOrderList;
+
+    private List<Image> imagesBackground;
     //public GameObject DisplayBox;
     //public GameObject PassBox;
     public int indexPlayer;
@@ -50,6 +61,7 @@ public class QTE : MonoBehaviour
 
         QTEGenList = new List<int>();
         QTEOrderList = new List<Image>();
+        imagesBackground = new List<Image>();
 
         increment = 0;
         
@@ -61,26 +73,51 @@ public class QTE : MonoBehaviour
         B_Btn = Resources.Load("UI/B_Btn", typeof(Sprite)) as Sprite;
         A_Btn = Resources.Load("UI/A_Btn", typeof(Sprite)) as Sprite;
 
-        FirstBtn = gameObject.transform.GetChild(0).GetChild(0).GetComponent<Image>();
+        FirstBtn = gameObject.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>();
+        BackFirstBtn = gameObject.transform.GetChild(0).GetChild(0).GetComponent<Image>();
         FirstBtn.CrossFadeAlpha(0,0, false);
-        SecondBtn = gameObject.transform.GetChild(0).GetChild(1).GetComponent<Image>();
+        SecondBtn = gameObject.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>();
+        BackSecondBtn = gameObject.transform.GetChild(0).GetChild(1).GetComponent<Image>();
         SecondBtn.CrossFadeAlpha(0,0, false);
-        ThirdBtn = gameObject.transform.GetChild(0).GetChild(2).GetComponent<Image>();
+        ThirdBtn = gameObject.transform.GetChild(0).GetChild(2).GetChild(0).GetComponent<Image>();
+        BackThirdBtn = gameObject.transform.GetChild(0).GetChild(2).GetComponent<Image>();
         ThirdBtn.CrossFadeAlpha(0, 0, false);
-        FourthBtn = gameObject.transform.GetChild(0).GetChild(3).GetComponent<Image>();
+        FourthBtn = gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetComponent<Image>();
+        BackFourthBtn = gameObject.transform.GetChild(0).GetChild(3).GetComponent<Image>();
         FourthBtn.CrossFadeAlpha(0,0, false);
-        FirstBtn1 = gameObject.transform.GetChild(0).GetChild(4).GetComponent<Image>();
+        FirstBtn1 = gameObject.transform.GetChild(0).GetChild(4).GetChild(0).GetComponent<Image>();
+        BackFirstBtn1 = gameObject.transform.GetChild(0).GetChild(4).GetComponent<Image>();
         FirstBtn1.CrossFadeAlpha(0,0, false);
-        SecondBtn1 = gameObject.transform.GetChild(0).GetChild(5).GetComponent<Image>();
+        SecondBtn1 = gameObject.transform.GetChild(0).GetChild(5).GetChild(0).GetComponent<Image>();
+        BackSecondBtn1 = gameObject.transform.GetChild(0).GetChild(5).GetComponent<Image>();
         SecondBtn1.CrossFadeAlpha(0,0, false);
-        ThirdBtn1 = gameObject.transform.GetChild(0).GetChild(6).GetComponent<Image>();
+        ThirdBtn1 = gameObject.transform.GetChild(0).GetChild(6).GetChild(0).GetComponent<Image>();
+        BackThirdBtn1 = gameObject.transform.GetChild(0).GetChild(6).GetComponent<Image>();
         ThirdBtn1.CrossFadeAlpha(0,0, false);
-        FourthBtn1 = gameObject.transform.GetChild(0).GetChild(7).GetComponent<Image>();
+        FourthBtn1 = gameObject.transform.GetChild(0).GetChild(7).GetChild(0).GetComponent<Image>();
+        BackFourthBtn1 = gameObject.transform.GetChild(0).GetChild(7).GetComponent<Image>();
         FourthBtn1.CrossFadeAlpha(0,0, false);
+        
+        imagesBackground.Add(BackFirstBtn);
+        imagesBackground.Add(BackSecondBtn);
+        imagesBackground.Add(BackThirdBtn);
+        imagesBackground.Add(BackFourthBtn);
+        imagesBackground.Add(BackFirstBtn1);
+        imagesBackground.Add(BackSecondBtn1);
+        imagesBackground.Add(BackThirdBtn1);
+        imagesBackground.Add(BackFourthBtn1);
+
+        foreach (Image image in imagesBackground)
+        { 
+            image.CrossFadeAlpha(0,0,false);
+        }
+
+        
         
         GetComponent<Renderer>().material = red;
 
         CreateList(indexPlayer);
+        RecreateBackList(indexPlayer);
         /////////////////////////////////////////////
         //GenerateSequence();
     }
@@ -100,6 +137,25 @@ public class QTE : MonoBehaviour
             QTEOrderList.Add(SecondBtn1);
             QTEOrderList.Add(ThirdBtn1);
             QTEOrderList.Add(FourthBtn1);
+        }
+    }
+    
+    void RecreateBackList(int index)
+    {
+        imagesBackground.Clear();
+        if (index == 0)
+        {
+            imagesBackground.Add(BackFirstBtn);
+            imagesBackground.Add(BackSecondBtn);
+            imagesBackground.Add(BackThirdBtn);
+            imagesBackground.Add(BackFourthBtn);
+        }
+        else if (index == 1)
+        {
+            imagesBackground.Add(BackFirstBtn1);
+            imagesBackground.Add(BackSecondBtn1);
+            imagesBackground.Add(BackThirdBtn1);
+            imagesBackground.Add(BackFourthBtn1);
         }
     }
     
@@ -139,8 +195,9 @@ public class QTE : MonoBehaviour
         QTEGenList.Clear();
         for (int i = 0; i < 4; i++)
         {
-            QTEOrderList[i].GameObject().SetActive(true);
+            //QTEOrderList[i].GameObject().SetActive(true);
             QTEOrderList[i].CrossFadeAlpha(255, 0.1f, false);
+            imagesBackground[i].CrossFadeAlpha(255, 0.1f, false);
             QTEOrderList[i].sprite = GenerateInput();
         }
     }
@@ -247,7 +304,9 @@ public class QTE : MonoBehaviour
         switch (CorrectKey)
         {
             case 1 :
-                QTEOrderList[increment].GameObject().SetActive(false);
+                QTEOrderList[increment].CrossFadeAlpha(0,0,false);
+                imagesBackground[increment].CrossFadeAlpha(0,0,false);
+                //QTEOrderList[increment].SetActive(false);
                 CorrectKey = 0;
                 // !!!!!! Son Click
                 increment += 1;
