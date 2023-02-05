6dbfc0289b6508c60cc8de1fd1d822f6629271d6
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-02-05 04:58:47 +0100

    Update Deplacment.cs

--- Assets/Scripts/Deplacment.cs
+++ Assets/Scripts/Deplacment.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using SplineMesh;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 using Random = UnityEngine.Random;
 
