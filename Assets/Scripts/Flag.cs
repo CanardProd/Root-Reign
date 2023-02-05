065d02904b12737b2889113acb9ad538964acca5
Drawnight01 <71324732+Drawnight01@users.noreply.github.com>
2023-02-05 12:29:00 +0100

    cc

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -27,6 +27,7 @@ public class Flag : MonoBehaviour
         {
             if (playerCapture != other.GetComponent<Deplacment>())
             {
+                transform.GetComponent<QTE>().ListCreation();
                 other.GetComponent<Deplacment>().isCapturing = true;
                 transform.GetComponent<QTE>().indexPlayer = other.GetComponent<Deplacment>().indexPlayer;
                 transform.GetComponent<QTE>().GenerateSequence();
