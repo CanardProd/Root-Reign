f3eba7d89011beed54a9aa61e46fff53d96853bc
Sebastien chevallier <44536776+The-Real-Seb@users.noreply.github.com>
2023-02-05 15:02:18 +0100

    zerefzfefez

---
+++ Assets/LoadScenePlay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenePlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hinput.anyGamepad.anyInput)
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+    }
+}
